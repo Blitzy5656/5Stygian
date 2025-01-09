19371ffe81f2c5a4fcdcc793dbca7dff1157404b
5Stygian <121655781+Blitzy5656@users.noreply.github.com>
2025-01-09 13:26:13 -0800

    Update thegame.js

--- scripts/thegame.js
+++ scripts/thegame.js
@@ -1,3 +1,12 @@
+lossChecker = 0
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 function theReminder() {
-    window.alert("You lost the game!")
+    lossChecker = getRngInteger(0,1000001)
+    if (lossChecker = 555555) {
+        window.alert("You lost the game!");
+    }
 }
