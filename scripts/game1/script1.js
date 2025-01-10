6a5dc6cd7ac84b8d27a9d599f0ebe3cb68a36f41
5Stygian <121655781+Blitzy5656@users.noreply.github.com>
2025-01-10 11:09:56 -0800

    Update script1.js

--- scripts/game1/script1.js
+++ scripts/game1/script1.js
@@ -1,1 +1,11 @@
+let points = 0;
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+while (true) {
+    document.getElementById("pointCounter").innerHTML = points;
+    sleep(1000);
+    points++;
+}
