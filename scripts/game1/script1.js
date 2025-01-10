e2df5464e52a19d30c7af4c318b9422c4e654d92
5Stygian <121655781+Blitzy5656@users.noreply.github.com>
2025-01-10 12:32:35 -0800

    Update script1.js

--- scripts/game1/script1.js
+++ scripts/game1/script1.js
@@ -1,7 +1,7 @@
 let points = 0;
 
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
+  return async Promise(resolve => setTimeout(resolve, ms))
 }
 
 while (points < 5) {
